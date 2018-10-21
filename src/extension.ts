a46dea98c5432c2d44be0dee7fc5db77d4c5d1d0
Stephane Levesque <stef.levesque@gmail.com>
2018-10-21 13:50:01 -0400

    :recycle: refactor to use async/await

--- src/extension.ts
+++ src/extension.ts
@@ -29,33 +29,30 @@ export function activate(context: vscode.ExtensionContext) {
     let provider = new LinkContentProvider();
     let registration = vscode.workspace.registerTextDocumentContentProvider(linkScheme, provider);
 
-    let disposable0 = vscode.commands.registerCommand('extension.previewLink', () => {
+    let disposable0 = vscode.commands.registerCommand('extension.previewLink', async () => {
         let ibo = <vscode.InputBoxOptions>{
             prompt: "url to preview",
             placeHolder: "https://..."
         }
-        vscode.window.showInputBox(ibo).then( async (link) => {
-            let webPanel = vscode.window.createWebviewPanel('PreviewHtml', 'Preview Link', -1, {enableFindWidget: true, enableScripts: true });
-            const response = await got(link);
-            webPanel.webview.html = response.body;
-            webPanel.reveal();
-        });
+        const link = await vscode.window.showInputBox(ibo);
+        let webPanel = vscode.window.createWebviewPanel('PreviewHtml', 'Preview Link', -1, {enableFindWidget: true, enableScripts: true });
+        const response = await got(link);
+        webPanel.webview.html = response.body;
+        webPanel.reveal();
     });
 
-    let disposable1 = vscode.commands.registerCommand('extension.openLink', () => {
+    let disposable1 = vscode.commands.registerCommand('extension.openLink', async () => {
         var ibo = <vscode.InputBoxOptions>{
             prompt: "url to download",
             placeHolder: "https://..."
         }
-        vscode.window.showInputBox(ibo).then( link => {
-            var uri = vscode.Uri.parse(link).with({ scheme: linkScheme});
-            vscode.workspace.openTextDocument(uri).then(doc => {
-                vscode.window.showTextDocument(doc);
-            });
-        });
+        const link = await vscode.window.showInputBox(ibo);
+        var uri = vscode.Uri.parse(link).with({ scheme: linkScheme});
+        const doc = await vscode.workspace.openTextDocument(uri)
+        vscode.window.showTextDocument(doc);
     });
 
-    let disposable2 = vscode.commands.registerCommand('extension.downloadLink', () => {
+    let disposable2 = vscode.commands.registerCommand('extension.downloadLink', async () => {
         if (vscode.workspace.rootPath == undefined) {
             return;
         }
@@ -64,85 +61,83 @@ export function activate(context: vscode.ExtensionContext) {
             placeHolder: "https://...",
             ignoreFocusOut: true
         }
-        vscode.window.showInputBox(ibo).then(link => {
-            if (!link) {
-                return;
-            }
-            var uri = vscode.Uri.parse(link);
-            ibo.value = path.basename(uri.toString());
-            ibo.prompt = "filename";
-            ibo.placeHolder = "filename";
-            vscode.window.showInputBox(ibo).then(filename => {
-                var filepath = path.join(vscode.workspace.rootPath, filename)
-                vscode.window.withProgress({
-                    location: vscode.ProgressLocation.Notification,
-                    title: "downloading " + filename,
-                    cancellable: true
-                }, (progress, token) => {
-                    let percent = 0;
-                    let transferred = 0;
-                    progress.report({ increment: 0 });
-                    return new Promise(resolve => {
-                        let cancelled = false;
+        const link = await vscode.window.showInputBox(ibo);
+        if (!link) {
+            return;
+        }
+        var uri = vscode.Uri.parse(link);
+        ibo.value = path.basename(uri.toString());
+        ibo.prompt = "filename";
+        ibo.placeHolder = "filename";
+        const filename = await vscode.window.showInputBox(ibo);
+        var filepath = path.join(vscode.workspace.rootPath, filename);
+        vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "downloading " + filename,
+            cancellable: true
+        }, (progress, token) => {
+            let percent = 0;
+            let transferred = 0;
+            progress.report({ increment: 0 });
+            return new Promise(resolve => {
+                let cancelled = false;
 
-                        let stream = got.stream(uri.toString(), {timeout: {connect: 10000, socket: 10000}});
-                        let fileStream = stream.pipe(fs.createWriteStream(filepath + '.download'));
-                        
-                        token.onCancellationRequested(() => {
-                            cancelled = true;
-                            stream.pause();
-                            stream.end(() => {
-                                console.log('stream cancelled end');
-                                fileStream.close();
-                            });
-                        });
-
-                        stream.on('downloadProgress', p => {
-                            if (percent < Math.floor(p.percent * 100)) {
-                                percent = Math.floor(p.percent * 100);
-                                let incr = p.total > 0 ? (p.transferred - transferred) / p.total : 0;
-                                progress.report({increment: incr * 100, message: `${Math.floor(percent)}%`})
-                                transferred = p.transferred;
-                            }
-                        });
-
-                        fileStream.on('close', () => {
-                            const yes: vscode.MessageItem = { title: "Yes" };
-                            const no: vscode.MessageItem = { isCloseAffordance: true, title: "No" };
-                            if (cancelled) {
-                                vscode.window.showWarningMessage("Download cancelled. Delete file?", yes, no).then(v => {
-                                    if (v === yes) {
-                                        fs.unlink(filepath + '.download', (e) => {
-                                            if (e) {
-                                                console.log(e);
-                                            }
-                                            resolve();
-                                        });
-                                    }
-                                });
-                            } else {
-                                fs.rename(filepath + '.download', filepath, (err) => {
-                                    if (err) {
-                                        vscode.window.showErrorMessage("Could not rename file \"" + filepath + "\"");
-                                    } 
-                                    else {
-                                        vscode.window.showInformationMessage("Download completed. Open file?", yes, no).then(v => {
-                                            if (v === yes) {
-                                                let fileUri = vscode.Uri.file(filepath);
-                                                vscode.commands.executeCommand('vscode.open', fileUri);
-                                            }
-                                        });
-                                    }
-
-                                    resolve();
-                                });
-                            }
-                        });
+                let stream = got.stream(uri.toString(), {timeout: {connect: 10000, socket: 10000}});
+                let fileStream = stream.pipe(fs.createWriteStream(filepath + '.download'));
+                
+                token.onCancellationRequested(() => {
+                    cancelled = true;
+                    stream.pause();
+                    stream.end(() => {
+                        console.log('stream cancelled end');
+                        fileStream.close();
                     });
+                });
+
+                stream.on('downloadProgress', p => {
+                    if (percent < Math.floor(p.percent * 100)) {
+                        percent = Math.floor(p.percent * 100);
+                        let incr = p.total > 0 ? (p.transferred - transferred) / p.total : 0;
+                        progress.report({increment: incr * 100, message: `${Math.floor(percent)}%`})
+                        transferred = p.transferred;
+                    }
+                });
+
+                fileStream.on('close', async () => {
+                    const yes: vscode.MessageItem = { title: "Yes" };
+                    const no: vscode.MessageItem = { isCloseAffordance: true, title: "No" };
+                    if (cancelled) {
+                        const v = await vscode.window.showWarningMessage("Download cancelled. Delete file?", yes, no);
+                        if (v === yes) {
+                            fs.unlink(filepath + '.download', (e) => {
+                                if (e) {
+                                    console.log(e);
+                                }
+                                resolve();
+                            });
+                        }
+                    } else {
+                        fs.rename(filepath + '.download', filepath, async (err) => {
+                            if (err) {
+                                vscode.window.showErrorMessage("Could not rename file \"" + filepath + "\"");
+                            } 
+                            else {
+                                const v = await vscode.window.showInformationMessage("Download completed. Open file?", yes, no)
+                                if (v === yes) {
+                                    let fileUri = vscode.Uri.file(filepath);
+                                    vscode.commands.executeCommand('vscode.open', fileUri);
+                                }
+                            }
+
+                            resolve();
+                        });
+                    }
                 });
             });
         });
     });
+
+
 
     let disposable3 = vscode.commands.registerCommand('extension.openPath', () => {
         var ibo = <vscode.InputBoxOptions>{
